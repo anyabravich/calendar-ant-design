8893145de9f5c121c761f6c7d3661f12ac516103
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-09-04 02:11:06 +0300

    Upd code

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -5,4 +5,4 @@ import { allActionCreators } from "../store/reducers/action-creators";
 export const useActions = () => {
   const dispatch = useDispatch();
   return bindActionCreators(allActionCreators, dispatch);
-}
+};
